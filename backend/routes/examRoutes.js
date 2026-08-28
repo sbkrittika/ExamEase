2415df5be85dce0bae9996cab42454de8a848115
Shafiya Bashar <sbkrittika@gmail.com>
2026-08-28 17:28:59 +0600

    Update examRoutes.js

--- backend/routes/examRoutes.js
+++ backend/routes/examRoutes.js
@@ -1,31 +1,70 @@
-const express = require("express");
+const express = require('express');
 const multer = require('multer');
+
 const upload = multer();
 
 const {
-    addExam,
-    getExams,
-    updateExam,
-    deleteExam,
-    uploadZip,
-    allocate,
-    getAllocations
-} = require("../controllers/examController");
-const { authenticate, allowRoles } = require("../middleware/auth");
+  addExam,
+  getExams,
+  updateExam,
+  deleteExam,
+  uploadZip,
+  allocate,
+  getAllocations
+} = require('../controllers/examController');
 
-const router = express.Router();
+const {
+  authenticate,
+  allowRoles
+} = require('../middleware/auth');
+
+const router =
+  express.Router();
 
 router.use(authenticate);
-router.post("/", allowRoles("faculty"), addExam);
-router.get("/", allowRoles("faculty"), getExams);
-router.put("/:id", allowRoles("faculty"), updateExam);
-router.delete("/:id", allowRoles("faculty"), deleteExam);
 
-// Upload a ZIP containing an .xlsx student list (multipart/form-data file field: file)
-router.post('/upload-zip', allowRoles("faculty"), upload.single('file'), uploadZip);
+router.post(
+  '/',
+  allowRoles('faculty'),
+  addExam
+);
 
-// Allocate students to rooms
-router.post('/allocate', allowRoles("faculty"), allocate);
-router.get("/:id/allocations", allowRoles("faculty"), getAllocations);
+router.get(
+  '/',
+  allowRoles('faculty'),
+  getExams
+);
 
+router.put(
+  '/:id',
+  allowRoles('faculty'),
+  updateExam
+);
+
+router.delete(
+  '/:id',
+  allowRoles('faculty'),
+  deleteExam
+);
+
+router.post(
+  '/upload-zip',
+  allowRoles('faculty'),
+  upload.single('file'),
+  uploadZip
+);
+
+router.post(
+  '/allocate',
+  allowRoles('faculty'),
+  allocate
+);
+
+router.get(
+  '/:id/allocations',
+  allowRoles('faculty'),
+  getAllocations
+);
+
+module.exports = router;
 module.exports = router;
